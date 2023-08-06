0da85838f83a80e2e732340249e7fa6ca9799b4b
athul8550 <athulmuralidharan2@gmail.com>
2023-08-06 20:21:03 +0530

    S1

---
+++ FrontEnd/scalpon/src/components/SellCard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./SellCard.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function SellCard(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
