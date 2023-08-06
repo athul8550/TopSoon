2bd7baf4b6d10bd1bec04cb8bb76448ffd5263d1
nirajvijay <chunjuv@gmail.com>
2023-08-06 20:38:26 +0530

    C4

--- FrontEnd/scalpon/src/components/CeCard.js
+++ FrontEnd/scalpon/src/components/CeCard.js
@@ -7,6 +7,9 @@ export default function CeCard(){
     return(
         <div>
           <h1 className="CeHeading">CE</h1>
+          <label className="ExpLabel" htmlFor="CeStrikePrice">
+        EXP
+      </label>
           <button className="BuyButtonCe" onClick={handleBuyBtClick}>
         BUY
       </button> 
