e48e22a223feea20c041b1860cc1017b0bec0096
nirajvijay <chunjuv@gmail.com>
2023-08-06 20:32:33 +0530

    C1

--- FrontEnd/scalpon/src/components/CeCard.js
+++ FrontEnd/scalpon/src/components/CeCard.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./BuyCard.css";
+import "./CeCard.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function BuyCard(){
+export default function CeCard(){
     return(
         <div>
-            
+          <button className="BuyButtonCe" onClick={handleBuyBtClick}>
+        BUY
+      </button>  
         </div>
     )
 }
