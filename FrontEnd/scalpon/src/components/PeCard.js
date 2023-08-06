cfad01db812589b4a916c6366aa9e474b9dc6420
athul8550 <athulmuralidharan2@gmail.com>
2023-08-06 20:32:09 +0530

    P1

--- FrontEnd/scalpon/src/components/PeCard.js
+++ FrontEnd/scalpon/src/components/PeCard.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./SellCard.css";
+import "./PeCard.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function SellCard(){
+export default function PeCard(){
     return(
         <div>
-            
+            <button className="SellButtonPe" onClick={handleBuyBtClick}>
+        BUY
+      </button>
         </div>
     )
 }
