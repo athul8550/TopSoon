1a3954191bcfb59d50927deb73897535c4f600ca
nirajvijay <chunjuv@gmail.com>
2023-08-06 20:20:52 +0530

    B1

---
+++ FrontEnd/scalpon/src/components/BuyCard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./BuyCard.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function BuyCard(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
